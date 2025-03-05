b42f1a2bfb45949ce789a4d7b4d3fdec97712448
Mark Louie <markllouie00@gmail.com>
2025-03-05 09:16:54 +0800

    Configure for deployment

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
 
+require('dotenv').config();
+
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
